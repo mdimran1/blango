6a07bfadeca562d0bfedaf881eef45b9e55841fe
Md Imran <captcha1030@gmail.com>
2023-09-20 16:14:03 +0000

    Finidh crispy form

--- blango/settings.py
+++ blango/settings.py
@@ -33,6 +33,10 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CRISPY_ALLOWED_TEMPLATE_PACK = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
 
 
 # Application definition
@@ -45,7 +49,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
+
+
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -33,30 +33,6 @@ def author_details(author, current_user=None):
     return format_html('{}{}{}', prefix, name, suffix)
 
 
-@register.simple_tag(takes_context=True)
-def author_details_tag(context):
-    request = context["request"]
-    current_user = request.user
-    post = context["post"]
-    author = post.author
-
-    if author == current_user:
-        return format_html("<strong>me</strong>")
-
-    if author.first_name and author.last_name:
-        name = f"{author.first_name} {author.last_name}"
-    else:
-        name = f"{author.username}"
-
-    if author.email:
-        prefix = format_html('<a href="mailto:{}">', author.email)
-        suffix = format_html("</a>")
-    else:
-        prefix = ""
-        suffix = ""
-
-    return format_html("{}{}{}", prefix, name, suffix)
-
 
 
 @register.simple_tag
